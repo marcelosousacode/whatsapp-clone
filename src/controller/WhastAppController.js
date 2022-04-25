9561e1bd57a418174d678044d013d35816a1cd5b
Marcelo Sousa <marcelosousacode@gmail.com>
2022-04-25 13:56:00 -0300

    Now, when logging in, the photo and username of the selected profile are received and it is possible to change the name through the application.

--- src/controller/WhastAppController.js
+++ src/controller/WhastAppController.js
@@ -3,6 +3,7 @@ import {CameraController} from './CameraController';
 import {MicrophoneController} from './MicrophoneController.js';
 import {DocumentPreviewController} from './DocumentPreviewController';
 import { Firebase } from './../util/Firebase';
+import { User } from '../model/User';
 
 
 export default class WhatsAppController {
@@ -20,12 +21,41 @@ export default class WhatsAppController {
 
     initAuth() {
 
-        this._firebase.initAuth().then((response, token)=> {
+        this._firebase.initAuth().then(response=> {
 
-            this._user = response.user;
+            this._user = new User(response.user.email);
 
-            this.el.appContent.css({
-                display:'flex',
+            this._user.on('datachange', data => {
+
+                document.querySelector('title').innerHTML = data.name + ' - WhastApp Clone'
+
+                this.el.inputNamePanelEditProfile.innerHTML = data.name;
+
+                if (data.photo) {
+
+                    let photo = this.el.imgPanelEditProfile;
+                    photo.src = data.photo;
+                    photo.show();
+                    this.el.imgDefaultPanelEditProfile.hide();
+
+                    let photo2 = this.el.myPhoto.querySelector('img')
+                    photo2.src = data.photo;
+                    photo2.show();
+
+                }
+
+            })
+
+            this._user.name  = response.user.displayName;
+            this._user.email  = response.user.email;
+            this._user.photo  = response.user.photoURL;
+
+            this._user.save().then(()=> {
+                
+                this.el.appContent.css({
+                    display:'flex',
+                });
+
             });
 
         }).catch(err=> {
@@ -205,8 +235,15 @@ export default class WhatsAppController {
 
         this.el.btnSavePanelEditProfile.on('click', e=> {
 
-            console.log( this.el.inputNamePanelEditProfile.innerHTML )
+            this.el.btnSavePanelEditProfile.disabled = true;
 
+            this._user.name = this.el.inputNamePanelEditProfile.innerHTML
+
+            this._user.save().then(()=>{
+
+                this.el.btnSavePanelEditProfile.disabled = false;
+
+            });
         })
 
         this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item=> {
